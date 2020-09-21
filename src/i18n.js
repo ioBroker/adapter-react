00662b55749b3f7c4dc7cca05e51ea2140bfadce
Samuel Weibel <samuel.weibel@gmail.com>
2020-09-21 10:35:33 +0200

    Added typing to i18n

--- src/i18n.js
+++ src/i18n.js
@@ -1,28 +1,61 @@
-/**
+/***
  * Copyright 2018-2019 bluefox <dogafox@gmail.com>
  *
  * MIT License
  *
- **/
+ ***/
 
+ /**
+  * Translation string management.
+  */
 class I18n {
+    /**
+     * List of all languages with their translations.
+     * @type {Record<string, Record<string, string>>}
+     */
     static translations = {};
 
+    /**
+     * The currently displayed language.
+     * @type {string}
+     */
     static lang = window.sysLang || 'en';
 
+    /**
+     * Set the language to display.
+     * @param {string} lang
+     */
     static setLanguage(lang) {
         if (lang) {
             I18n.lang = lang;
         }
     }
+
+    /**
+     * Sets all translations (in all languages).
+     * @param {Record<string, Record<string, string>>} translations
+     */
     static setTranslations(translations) {
         if (translations) {
             I18n.translations = translations;
         }
     }
+
+    /**
+     * Get the currently chosen language.
+     * @returns {string} The current language.
+     */
     static getLanguage() {
         return I18n.lang;
     }
+
+    /**
+     * Translate the given string to the selected language.
+     * @param {string} word The (key) word to look up the string.
+     * @param {string | undefined} [arg1] Optional argument which will replace the first occurence of %s
+     * @param {string | undefined} [arg2] Optional argument which will replace the second occurence of %s
+     * @param {string | undefined} [arg3] Optional argument which will replace the thrid occurence of %s
+     */
     static t(word, arg1, arg2, arg3) {
         if (I18n.translations[I18n.lang]) {
             const w = I18n.translations[I18n.lang][word];
