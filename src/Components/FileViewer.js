81ea69afb667a89c5fe089a0653b306c282e769f
GermanBluefox <dogafox@gmail.com>
2020-09-22 19:49:03 +0200

    Added File Browser

---
+++ src/Components/FileViewer.js
@@ -0,0 +1,144 @@
+import React from 'react';
+import {withStyles} from '@material-ui/core/styles';
+import withWidth from "@material-ui/core/withWidth";
+import PropTypes from 'prop-types';
+
+import Button from '@material-ui/core/Button';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import TextField from '@material-ui/core/TextField';
+
+import NoImage from '../assets/no_icon.svg';
+import Utils from './Utils';
+
+// Icons
+import {FaCopy as CopyIcon} from 'react-icons/fa';
+import CloseIcon from '@material-ui/icons/Close';
+
+const styles = theme => ({
+    dialog: {
+        height: '100%',
+        maxHeight: '100%',
+        maxWidth: '100%',
+    },
+    content: {
+        textAlign: 'center',
+    },
+    textarea: {
+        width: '100%',
+        height: '100%',
+    },
+    img: {
+        width: 'auto',
+        height: 'calc(100% - 5px)',
+        objectFit: 'contain',
+    }
+});
+
+export const EXTENSIONS = {
+    images: ['png', 'jpg', 'svg', 'jpeg', 'jpg'],
+    code:   ['js', 'json'],
+    txt:    ['log', 'txt', 'html', 'css', 'xml'],
+};
+
+function getFileExtension(fileName) {
+    const pos = fileName.lastIndexOf('.');
+    if (pos !== -1) {
+        return fileName.substring(pos + 1).toLowerCase();
+    } else {
+        return null;
+    }
+}
+
+class FileViewer extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.ext = getFileExtension(this.props.href); // todo: replace later with Utils.getFileExtension
+
+        this.state = {
+            text: null,
+            code: null,
+            copyPossible: EXTENSIONS.code.includes(this.ext) || EXTENSIONS.txt.includes(this.ext)
+        };
+
+        if (this.state.copyPossible) {
+            fetch(this.props.href)
+                .then(response => response.text())
+                .then(data => {
+                    if (EXTENSIONS.txt.includes(this.ext)) {
+                        this.setState({text: data});
+                    } else if (EXTENSIONS.code.includes(this.ext)) {
+                        this.setState({code: data});
+                    }
+                });
+        }
+    }
+
+    static getDerivedStateFromProps() {
+
+    }
+
+    getContent() {
+        if (EXTENSIONS.images.includes(this.ext)) {
+            return <img
+                onError={ e => {
+                    e.target.onerror = null;
+                    e.target.src = NoImage
+                } }
+                className={ this.props.classes.img }
+                src={ this.props.href } alt={ this.props.href }/>;
+        } else if (this.state.code !== null) {
+            return <TextField
+                className={ this.props.classes.textarea }
+                multiline
+                value={ this.state.code }
+                readOnly={true}/>;
+        } else  if (this.state.text !== null) {
+            return <TextField
+                className={ this.props.classes.textarea }
+                value={ this.state.code }
+                multiline
+                readOnly={true}/>;
+        }
+    }
+
+    render() {
+        return <Dialog
+            className={ this.props.classes.dialog }
+            open={ this.props.href }
+            onClose={ () => this.props.onClose() }
+            fullWidth={ true }
+            fullScreen={ true }
+            aria-labelledby="form-dialog-title"
+        >
+            <DialogTitle id="form-dialog-title">{ this.props.t('View: %s', this.props.href) }</DialogTitle>
+            <DialogContent className={ this.props.classes.content }>
+                    { this.getContent() }
+            </DialogContent>
+            <DialogActions>
+                { this.state.copyPossible ? <Button onClick={e => Utils.copyToClipboard(this.state.text || this.state.code, e) } >
+                    <CopyIcon />
+                    { this.props.t('Copy content') }
+                </Button> : null }
+
+                <Button onClick={() => this.props.onClose()} color="primary">
+                    <CloseIcon />
+                    { this.props.t('Close') }
+                </Button>
+            </DialogActions>
+        </Dialog>;
+    }
+}
+
+FileViewer.propTypes = {
+    t: PropTypes.func,
+    lang: PropTypes.string,
+    expertMode: PropTypes.bool,
+    onClose: PropTypes.func,
+    href: PropTypes.string.isRequired
+};
+
+export default withWidth()(withStyles(styles)(FileViewer));
---
+++ src/Dialogs/IconPicker.js
@@ -0,0 +1,319 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import {withStyles} from '@material-ui/core/styles';
+import clsx from 'clsx';
+
+import TextField from '@material-ui/core/TextField';
+import I18n from '@iobroker/adapter-react/i18n';
+import IconButton from '@material-ui/core/IconButton';
+import Button from '@material-ui/core/Button';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import Grid from '@material-ui/core/Grid';
+
+import ClearIcon from '@material-ui/icons/Close';
+import CancelIcon from '@material-ui/icons/Close';
+import CheckIcon from '@material-ui/icons/Check';
+import SelectIcon from '@material-ui/icons/ViewModule';
+
+import Image from '../Components/Image';
+import FileBrowser from '../Components/FileBrowser';
+
+import IconLampTable from '../assets/lamp_table.svg';
+import IconLampCeiling from '../assets/lamp_ceiling.svg';
+
+const ICONS = [
+    {icon: IconLampTable,   color: '#FFFFFF', name: 'Table lamp'},
+    {icon: IconLampCeiling, color: '#FFFFFF', name: 'Ceiling lamp'},
+];
+
+const styles = theme => ({
+    div: {
+        width: '100%',
+        lineHeight: '48px',
+    },
+    dialogContent: {
+        overflow: 'hidden',
+    },
+    dialogTab: {
+        height: 500,
+        overflow: 'hidden',
+    },
+    textField: {
+
+    },
+    textFieldWithButton: {
+        width: 'calc(100% - 102px)',
+        verticalAlign: 'bottom'
+    },
+    textDense: {
+        marginTop: 0,
+        marginBottom: 0,
+    },
+    buttonIcon: {
+        marginRight: theme.spacing(1),
+    },
+    icon: {
+        width: 32,
+        height: 32,
+        margin: theme.spacing(1),
+        color: theme.palette.type === 'dark' ? '#FFF' : '#000'
+    },
+    grid: {
+        padding: theme.spacing(2)
+    },
+    gridIcon: {
+        '&:hover': {
+            background: theme.palette.primary.dark,
+        },
+        textAlign: 'center',
+    },
+    selectButton: {
+        verticalAlign: 'bottom'
+    },
+    iconSelected: {
+        background: theme.palette.primary.main,
+        '&:hover': {
+            background: theme.palette.primary.light,
+        }
+    },
+    imagePreviewDiv: {
+        display: 'inline-block',
+        marginRight: theme.spacing(1),
+        verticalAlign: 'bottom',
+    },
+    imagePreview: {
+        width: 32,
+        height: 32,
+        color: theme.palette.type === 'dark' ? '#FFF' : '#000',
+    }
+});
+
+const PRESET_PREFIX = 'preset:';
+
+class IconPicker extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            color: this.props.color || '',
+            value: this.props.value || '',
+            dialogValue: '',
+            showDialog: false,
+            selectedTab: this.props.value && !this.props.value.startsWith(PRESET_PREFIX) ? 1 : 0,
+            imgError: false,
+        };
+        this.imagePrefix = this.props.imagePrefix || './files/';
+    }
+
+    /*static getDerivedStateFromProps(props, state) {
+        const newState = {};
+        let changed = false;
+
+        if (props && state && props.value !== state.value) {
+            newState.value = props.value;
+            changed = true;
+        }
+
+        if (props && state && props.color !== state.color) {
+            newState.color = props.color;
+            changed = true;
+        }
+
+        return changed ? newState : null;
+    }*/
+
+    renderOneIcon(item, i) {
+        return <Grid
+             item
+             xs
+             key={item.name || i}
+             className={clsx(this.props.classes.gridIcon, this.state.dialogValue && this.state.dialogValue.startsWith(PRESET_PREFIX) && this.state.dialogValue.endsWith(item.name) && this.props.classes.iconSelected)}
+             onClick={() => this.setState({dialogValue: PRESET_PREFIX + item.name})}
+             onDoubleClick={() => this.setState({dialogValue: PRESET_PREFIX + item.name}, () => this.onDialogClose(this.state.dialogValue))}
+        >
+            <img className={this.props.classes.icon} src={item.icon} alt={item.name} style={item.color ? {color: item.color} : {}}/>
+        </Grid>;
+    }
+
+    renderPredefinedIcons() {
+        return <Grid container spacing={2} justify="center" className={this.props.classes.grid}>{
+            ICONS.map((item, i) => this.renderOneIcon(item, i))
+        }</Grid>;
+    }
+
+    fetchIcon(src) {
+        return fetch(src)
+            .then(response => response.text())
+    }
+
+    getIdFromSrc(svg) {
+        const len = 'data:image/svg+xml;base64, ';
+        if (!svg || !svg.startsWith(len)) {
+            return null;
+        }
+        svg = svg.substring(len.length);
+        try {
+            svg = atob(svg);
+            const m = svg.match(/<svg id="([^"]+)"/);
+            return m ? m[1] : null;
+        } catch (e) {
+            console.warn('Cannot decode ' + svg);
+        }
+        return null;
+    }
+
+    renderFileBrowser() {
+        return <FileBrowser
+            t={I18n.t}
+            imagePrefix={this.imagePrefix}
+            lang={I18n.getLanguage()}
+            socket={this.props.socket}
+            ready={true}
+            showExpertButton={true}
+            showToolbar={true}
+            allowUpload={true}
+            allowDownload={false}
+            allowCreateFolder={true}
+            allowDelete={false}
+            allowView={true}
+            showViewTypeButton={false}
+            filterFiles={['png', 'svg', 'bmp', 'jpg', 'jpeg']}
+            onSelect={(path, isDoubleClick) =>
+                this.setState({dialogValue: path}, () =>
+                    isDoubleClick && this.onDialogClose(path))}
+        />;
+    }
+
+    onDialogClose(value) {
+        if (value) {
+            if (value.startsWith(PRESET_PREFIX)) {
+                value = value.substring(7);
+                const item = ICONS.find(item => item.name === value);
+                if (item) {
+                    //fetch icon
+                    this.fetchIcon(item.icon)
+                        .then(svg => {
+                            svg = svg.replace('<svg ', '<svg id="' + value + '" ');
+                            const valueSvg = 'data:image/svg+xml;base64, ' + window.btoa(svg);
+                            this.setState({value: valueSvg, showDialog: false, imgError: false, dialogValue: '', dialogInitialValue: ''}, () =>
+                                this.props.onChange(valueSvg));
+                        });
+                } else {
+                    this.setState({value: '', showDialog: false, imgError: false, dialogValue: '', dialogInitialValue: ''}, () =>
+                        this.props.onChange(''));
+                }
+            } else {
+                // it is path to image
+                this.setState({value, showDialog: false, imgError: false, dialogValue: '', dialogInitialValue: ''}, () =>
+                    this.props.onChange(value));
+            }
+        } else {
+            this.setState({showDialog: false});
+        }
+    }
+
+    renderDialog() {
+        if (!this.state.showDialog) {
+            return null;
+        }
+        return <Dialog
+            open={true}
+            fullWidth
+            maxWidth="lg"
+            onClose={() => this.setState({showDialog: false})}
+        >
+            <DialogTitle>{I18n.t('Select icon...')}</DialogTitle>
+            <DialogContent className={this.props.classes.dialogContent}>
+                <Tabs value={this.state.selectedTab} onChange={(e, selectedTab) => this.setState({selectedTab})}>
+                    <Tab label={I18n.t('Predefined')} />
+                    <Tab label={I18n.t('User defined')} />
+                </Tabs>
+                {this.state.selectedTab === 0 && <div className={this.props.classes.dialogTab}>
+                    {this.renderPredefinedIcons()}
+                </div>}
+                {this.state.selectedTab === 1 && <div className={this.props.classes.dialogTab}>
+                    {this.renderFileBrowser()}
+                </div>}
+            </DialogContent>
+            <DialogActions>
+                <Button onClick={() => this.onDialogClose()}><CancelIcon className={this.props.classes.buttonIcon}/>{I18n.t('Cancel')}</Button>
+                <Button onClick={() => this.onDialogClose(this.state.dialogValue)}
+                        color="primary"
+                        disabled={!this.state.dialogValue || this.state.dialogInitialValue === this.state.dialogValue}
+                        autoFocus
+                >
+                    <CheckIcon className={this.props.classes.buttonIcon}/>{I18n.t('Select')}
+                </Button>
+            </DialogActions>
+        </Dialog>
+    }
+
+    render() {
+        return <div
+            style={this.props.style || {}}
+            className={ clsx(this.props.classes.div, this.props.className)}
+        >
+            <div className={this.props.classes.imagePreviewDiv}>
+                <Image
+                    imagePrefix={this.imagePrefix}
+                    showError={true}
+                    color={this.state.color}
+                    className={this.props.classes.imagePreview}
+                    src={this.state.value}
+                    alt="preview"
+                />
+            </div>
+            <TextField
+                margin="dense"
+                label={this.props.label || I18n.t('Icon')}
+                value={this.state.value}
+                onChange={e => {
+                    const value = e.target.value;
+                    this.setState({value, imgError: false}, () => this.props.onChange(value));
+                }}
+                type="text"
+                InputProps={{
+                    endAdornment: this.state.value ? (
+                        <IconButton
+                            onClick={() => {
+                                this.setState({value: '', imgError: false}, () => this.props.onChange(''));
+                            }}>
+                            <ClearIcon />
+                        </IconButton>
+                    ) : undefined,
+                }}
+                className={this.props.classes.textFieldWithButton}
+            />
+            <IconButton
+                className={this.props.classes.selectButton}
+                onClick={() => {
+                    let id = this.getIdFromSrc(this.state.value);
+                    if (id) {
+                        id = PRESET_PREFIX + id;
+                    } else {
+                        id = this.state.value;
+                    }
+                    this.setState({showDialog: true, dialogValue: id, dialogInitialValue: id});
+                }}
+            >
+                <SelectIcon/>
+            </IconButton>
+            {this.renderDialog()}
+        </div>
+    }
+}
+
+IconPicker.propTypes = {
+    color: PropTypes.string,
+    value: PropTypes.string,
+    label: PropTypes.string,
+    onChange: PropTypes.func.isRequired,
+    socket: PropTypes.object.isRequired,
+    imagePrefix: PropTypes.string,
+};
+
+export default withStyles(styles)(IconPicker);
